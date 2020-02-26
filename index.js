3289aca26c4ebfab20b28075f30235d5535541aa
david@konecny.eu <david@konecny.eu>
2020-02-26 22:41:32 +0100

    Index fix

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 var proxy = require('express-http-proxy');
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3999;
 
-app.use('/blablabot', proxy('localhost:3004'));
 app.use('/apartment', proxy('localhost:3111'));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.get('/', (req, res) => res.send('davidkonecny.eu api index page'));
+
+app.listen(port, () => console.log(`Proxy server listening on ${port}!`));
